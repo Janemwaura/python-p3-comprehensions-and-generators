c31488b4ec17561f1186877057797774eed99806
Janemwaura <mwaura461@gmail.com>
2024-01-16 12:37:22 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [x for x in sequence if x % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = return_evens(numbers)
+print(result)
+
+
+
+
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+sentences = ["Hello, how are you", "I love programming", "Have a great day"]
+result = make_exclamation(sentences)
+print(result)
+
+def return_first(sequence):
+    return [x for x in sequence if x + 1 == 6]
+numbers =[4,8,16,12,6,5]
+result = return_first(numbers)
+print(result)
